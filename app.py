1edee9dfeb6939238e238567da819b88a92f7a19
Geddam Shyam Gowtham <137587586+nameishyam@users.noreply.github.com>
2024-10-07 22:51:21 +0530

    Refactor text transformation and prediction logic

--- app.py
+++ app.py
@@ -10,21 +10,26 @@ import string
 
 nltk.download('punkt_tab')
 
-ps = PorterStemmer()
+porter_stemmer = PorterStemmer()
 
 # Load the trained model
-tfidf = pickle.load(open('D:\\render\\vectorizer.pkl', 'rb'))
-model = pickle.load(open('D:\\render\\model.pkl', 'rb'))
+tfidf_vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+spam_model = pickle.load(open('model.pkl', 'rb'))
+
+def tokenize_text(text):
+    return nltk.word_tokenize(text.lower())
+
+def filter_tokens(tokens):
+    return [token for token in tokens if token.isalnum() and token not in stopwords.words('english') and token not in string.punctuation]
+
+def stem_tokens(tokens):
+    return [porter_stemmer.stem(token) for token in tokens]
 
 def transform_text(text):
-    text = text.lower()
-    words = nltk.word_tokenize(text)
-    
-    filtered_words = [word for word in words if word.isalnum()]
-    filtered_words = [word for word in filtered_words if word not in stopwords.words('english') and word not in string.punctuation]
-    stemmed_words = [ps.stem(word) for word in filtered_words]
-    
-    return " ".join(stemmed_words)
+    tokens = tokenize_text(text)
+    filtered_tokens = filter_tokens(tokens)
+    stemmed_tokens = stem_tokens(filtered_tokens)
+    return " ".join(stemmed_tokens)
 
 app = Flask(__name__)
 
@@ -34,23 +39,28 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Extract data from form
-    input_mail = request.form.get('mail_content')  # Assuming the form field is named 'email'
-    
-    if not input_mail:
-        return 'Invalid input', 400
-    
-    transformed_email = transform_text(input_mail)
-    vector_input = tfidf.transform([transformed_email])  # Remove the outer list
-    
-    # Make prediction
-    prediction = model.predict(vector_input)[0]
-    if prediction == 0:
-        output = 'Not Spam!'
-    else:
-        output = 'Spam!'
-
-    return render_template('index.html', prediction_text='Prediction: {}'.format(output))
+    try:
+        input_mail = request.form.get('mail_content')
+        if not input_mail:
+            return 'Invalid input', 400
+        
+        transformed_email = transform_text(input_mail)
+        print("Transformed Email:", transformed_email)
+        
+        vector_input = tfidf_vectorizer.transform([transformed_email])
+        print("Vectorized Input:", vector_input.toarray())
+        
+        prediction = spam_model.predict(vector_input)
+        print("Prediction:", prediction)
+        
+        if prediction == 0:
+            output = 'Not Spam!'
+        else:
+            output = 'Spam!'
+        
+        return render_template('index.html', prediction_text='Prediction: {}'.format(output))
+    except Exception as e:
+        return 'Error occurred: {}'.format(str(e))
 
 if __name__ == "__main__":
     app.run(debug=True)
